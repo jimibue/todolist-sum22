05e8788d930c24bc7d2c3f67d54e7c498a0ffe0f
James <jimibue@gmail.com>
2022-05-26 12:03:28 -0600

    state

--- src/Todo.js
+++ src/Todo.js
@@ -1,10 +1,16 @@
+import {useState} from 'react'
 const Todo = (props)=>{
-    
+    const [showButtons, setShowButtons] = useState(true)
     return (
         <li className={`todo ${props.complete ? "complete":''}`}>
             <p>{props.name} id:{props.id}</p>
-            <button onClick={()=> props.removeTodoYo(props.id)}>delete</button>
-            <button onClick={()=> props.updateTodoYo(props.id)}>update</button>
+            <button onClick={()=>setShowButtons(!showButtons)}>toggle</button>
+            {showButtons && (
+             <> 
+              <button onClick={()=> props.removeTodoYo(props.id)}>delete</button>
+              <button onClick={()=> props.updateTodoYo(props.id)}>update</button>
+            </>  
+            )}
         </li>
     )
 }
